b574ab12629df424230e6dfb39d66d6e04d4bd1c
qx-7 <62677879+qx-7@users.noreply.github.com>
2025-06-04 17:50:35 -0600

    learning elm progress

--- 2025-elm/beginning-elm/src/Playground.elm
+++ 2025-elm/beginning-elm/src/Playground.elm
@@ -28,6 +28,80 @@ escapeEarth myVelocity mySpeed fuelStatus =
         whereToLand
 
 
+weekday dayInNumber =
+    case dayInNumber of
+        0 ->
+            "Sunday"
+
+        1 ->
+            "Monday"
+
+        2 ->
+            "Tuesday"
+
+        3 ->
+            "Wednesday"
+
+        4 ->
+            "Thursday"
+
+        5 ->
+            "Friday"
+
+        6 ->
+            "Saturday"
+
+        _ ->
+            "Unknown day"
+
+
+hashtag dayInNumber =
+    case weekday dayInNumber of
+        "Sunday" ->
+            "#Suckday"
+
+        "Monday" ->
+            "#LasagnaNight"
+
+        "Tuesday" ->
+            "#MoreLasagna"
+
+        "Wednesday" ->
+            "#HumpDay"
+
+        "Thursday" ->
+            "#ThrowbackThursday"
+
+        "Friday" ->
+            "#Fartday"
+
+        "Saturday" ->
+            "#Shaturday"
+
+        _ ->
+            "#null"
+
+
+revelation =
+    """
+    It became very clear to me sitting out there today
+    that every decision I've made in my entire life has
+    been wrong. My life is the complete "opposite" of
+    everything I want it to be. Every instinct I have,
+    in every aspect of life, be it something to wear,
+    something to eat - it's all been wrong.
+    """
+
+
 main =
+    {--
     escapeEarth 10 6.7 "low"
         |> Html.text
+    --}
+    {--
+    Html.text <|
+        hashtag 5
+--}
+    {--}
+    Html.text revelation
+--}
